775a9224770a1f4df98a9247ef912fca1aef3209
COMPUTER\vasek <vasika@gmail.com>
2021-04-24 13:02:45 +0300

    01-iliabvf

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        // test
         SpringApplication.run(Main.class, args);
     }
 }
