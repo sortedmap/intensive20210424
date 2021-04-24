6689586242d78f99838ec73a8c344bffcacf8cd6
sortedmap <sortedmap@gmail.com>
2021-04-24 15:54:10 +0300

    deleted ignored files from cache

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("fsdfsd");
         SpringApplication.run(Main.class, args);
     }
 }
