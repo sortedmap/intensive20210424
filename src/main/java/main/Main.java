199b378240ae80854df520f49ec8741f0099025c
sortedmap <sortedmap@gmail.com>
2021-04-24 15:49:50 +0300

    added folders to .gitignore

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        System.out.println("fsdfsd");
         SpringApplication.run(Main.class, args);
     }
 }
