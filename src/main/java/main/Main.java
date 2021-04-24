32913d614e570c5197a58f9908cbd5dfbbaa82b1
Andrey <x137@yandex.ru>
2021-04-24 13:16:11 +0300

    test commit

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
+    //123
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
