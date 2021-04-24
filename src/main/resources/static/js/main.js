35016b64be0598ed06de9f4bdb24e1bafaf4221b
sortedmap <sortedmap@gmail.com>
2021-04-24 17:43:09 +0300

    auth frontend implemented

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,27 @@
 $(function(){
-    $.get('/api/auth')
+
+    let showInterface = function(login) {
+        $('.main').css('display', 'flex');
+        //TODO: display user name
+    };
+
+    let registerUser = function(login){
+        $.post('/api/auth', {"login": login}, function(response){
+            if(response.result) {
+                showInterface(login);
+            } else {
+                alert(response.error);
+            }
+        });
+    };
+
+    $.get('/api/auth', {}, function(response){
+        if(response.result) {
+            showInterface(response.login)
+        }
+        else {
+            let login = prompt('Введите Ваше имя:');
+            registerUser(login);
+        }
+    });
 });
